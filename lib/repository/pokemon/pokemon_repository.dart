41c922f46a02a0ba60bf649ced20dca6748f1d67
SatoruInoue2003 <satoru.inoue2003@gmail.com>
2023-11-22 19:54:22 +0900

    feat: DioでのAPI呼び出し

--- lib/repository/pokemon/pokemon_repository.dart
+++ lib/repository/pokemon/pokemon_repository.dart
@@ -1,8 +1,7 @@
 import 'dart:async';
-import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:http/http.dart' as http;
 import 'package:pokemon_book/core/string.dart';
 import 'package:pokemon_book/model/pokemon.dart';
 
@@ -12,27 +11,28 @@ class PokemonRepository {
   PokemonRepository();
 
   Future<AsyncValue<List<Pokemon>>> fetchPokemonList() async {
+    final dio = Dio();
     final pokemonList = <Pokemon>[];
 
-    for (var id = 1; id <= 151; id++) {
-      final res =
-          await http.get(Uri.parse('$pokeApiRoute/pokemon-species/$id'));
+    for (var id = 1; id <= 10; id++) {
+      final res = await dio.get('$pokeApiRoute/pokemon-species/$id');
 
       if (res.statusCode == 200) {
-        final result = jsonDecode(res.body);
-        final names = result['names'];
-        final jaName = names.firstWhere(
-            (name) => name['language']['name'] == 'ja',
-            orElse: () => null);
+        try {
+          var pokemon = Pokemon.fromJson(res.data);
 
-        String japaneseName = jaName['name'];
+          /// pokemonのnameをjapaneseNameに置き換える
+          final names = res.data['names'];
+          final jaName = names.firstWhere(
+              (name) => name['language']['name'] == 'ja',
+              orElse: () => null);
+          String japaneseName = jaName['name'];
+          pokemon = pokemon.copyWith(name: japaneseName);
 
-        var pokemon = Pokemon.fromJson(result);
-
-        /// pokemonのnameをjapaneseNameに置き換える
-        pokemon = pokemon.copyWith(name: japaneseName);
-
-        pokemonList.add(pokemon);
+          pokemonList.add(pokemon);
+        } catch (error) {
+          throw Exception(error);
+        }
       } else {
         throw Exception('Failed to Load Pokemon');
       }
